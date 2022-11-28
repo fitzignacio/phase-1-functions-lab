9d510fcc9732030c4d8c5e86c6ea1a25ee7b6f78
Fitz Ignacio <ignacio.fitz@gmail.com>
2022-11-27 19:34:34 -0800

    commit tree d796f78748c311728a1976d70513fa51eeff06a6

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    const hq = 42;
+    const distBlocks = Math.abs(blocks - 42);
+    return distBlocks;
+}
+
+function distanceFromHqInFeet(blocks) {
+    const blockLength = 264;
+    return distanceFromHqInBlocks(blocks) * blockLength;
+}    
+
+function distanceTravelledInFeet(start, destination) {
+    const blockLength = 264;
+    const distFeet = Math.abs(destination-start);
+    return distFeet * blockLength;
+  }
+
+function calculatesFarePrice(start, destination) {
+    const blockLength = 264;
+    const distFeet = Math.abs(destination-start) * blockLength;
+    let price = 0
+    if (distFeet <=400) {
+        price = 0
+        return price;
+    }
+    else if (distFeet > 400 && distFeet <= 2000) {
+        let price = (distFeet - 400) * .02;
+        return price;
+    }
+    else if (distFeet > 2000 && distFeet <= 2500) {
+        let price = 25;
+        return price;
+    }
+    else if (distFeet > 2500) {
+        let price = ("cannot travel that far");
+        return price
+  }
+}
